09a3efa4106c8079cbd7d61ce6748b593d6baf20
maxello <maxellort@gmail.com>
2019-03-19 01:59:18 +0200

    Added homework

---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Car } from './interfaces/car.interface';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public carInfo:Car = {
+    name: "Mazda",
+    mileage: 120000,
+    fuelTankCapacity: 60,
+    currentFuelCapacity: 27,
+    consumption: 10,
+    specifications: [
+      "Двигатель: 1.5 литра", 
+      "Максимальная скорость: 200 км/ч", 
+      "Разгон до 100км/ч: 11.2c", 
+      "Тип кузова: Седан" 
+    ]
+  }
+
+  public drive(kilometers:number):void {
+      if (this.carInfo.currentFuelCapacity > 0) {
+          if(this.carInfo.currentFuelCapacity < kilometers/this.carInfo.consumption){
+            this.carInfo.mileage += kilometers/this.carInfo.consumption * this.carInfo.currentFuelCapacity;
+            this.carInfo.currentFuelCapacity = 0;
+          } else {
+            this.carInfo.mileage += kilometers;
+            this.carInfo.currentFuelCapacity -= kilometers/this.carInfo.consumption;
+          }
+      }
+  }
+
+  public refuel(count:number):void {
+      this.carInfo.currentFuelCapacity += count;
+  }
+}
---
+++ src/app/components/car/interfaces/car.interface.ts
@@ -0,0 +1,8 @@
+export interface Car {
+    name: string;
+    mileage: number;
+    fuelTankCapacity: number;
+    consumption: number;
+    currentFuelCapacity: number;
+    specifications: string[];
+}
